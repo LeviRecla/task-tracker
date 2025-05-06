d7126511dd2784b26767fc8e9583efc53b719fdf
Levi <levirecla@u.boisestate.edu>
2025-05-06 11:00:38 -0600

    comment

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# task has an id, title, description, and status
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
